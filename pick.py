13e9ecd937ca45535078d6fcf40f8e816d88a09e
Holden Higgins <hhiggins@stuy.edu>
2017-09-16 22:59:01 -0400

    comments

--- pick.py
+++ pick.py
@@ -3,22 +3,25 @@ import sys
 import random
 
 occupations  = open("occupations.csv", "r")
-jobs = csv.reader(occupations)
-dictionary = {"Jobs":"" , "Percent":""}
+jobs = csv.reader(occupations)#module parses csv file
+dictionary = {"Jobs":[] , "Percent":[]}#master dictionary
 job_type = []
 percent = []
 
 for row in jobs:
     try:
+        #adds job type and percent to lists
         job_type.append(row[0])
         percent.append(float(row[1]))
     except:
-        pass 
+        pass
+
+#inputs lists of job type and percent into dictionary
 dictionary["Jobs"] = job_type
 dictionary["Percent"] = percent
 
 def random_job():
-    """generate a random number, add the percents, last one
+    """generate a random number, subtract the percents, last one
      to go over is the desired job"""
     num = random.random()*100
     i=-1
