4ffeacf0c8c8cf21e80fce33e105c92ef7290b9c
Holden Higgins <hhiggins@stuy.edu>
2017-09-16 22:41:56 -0400

    implememted picker

--- pick.py
+++ pick.py
@@ -5,19 +5,26 @@ import random
 occupations  = open("occupations.csv", "r")
 jobs = csv.reader(occupations)
 dictionary = {"Jobs":"" , "Percent":""}
-classes = []
+job_type = []
 percent = []
 
 for row in jobs:
     try:
-        classes.append(row[0])
+        job_type.append(row[0])
         percent.append(float(row[1]))
     except:
         pass 
-dictionary["Jobs"] = classes
+dictionary["Jobs"] = job_type
 dictionary["Percent"] = percent
 
 def random_job():
     """generate a random number, add the percents, last one
      to go over is the desired job"""
-    num = randint(0,
+    num = random.random()*100
+    i=-1
+    while num>0:
+        i+=1
+        num-=dictionary["Percent"][i]
+    return dictionary["Jobs"][i]
+        
+print(random_job())
